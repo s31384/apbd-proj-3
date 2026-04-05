af2fe17f45891a2af123d2bb0459fdc74ac0c3e7
Artem Kraiushkin <s31384@pjwstk.edu.pl>
2026-04-06 01:52:21 +0300

    now 4 separate methods for data validatiion in DataValidator class

--- LegacyRenewalApp/DataValidator.cs
+++ LegacyRenewalApp/DataValidator.cs
@@ -5,21 +5,38 @@ public class DataValidator : IDataValidator
 {
     public void ValidateData(int customerId, string planCode, int seatCount, string paymentMethod)
     {
+        ValidateId(customerId);
+        ValidatePlanCode(planCode);
+        ValidateSeatCount(seatCount);
+        ValidatePaymentMethod(paymentMethod);
+    }
+
+    void ValidateId(int customerId)
+    {
         if (customerId <= 0)
         {
             throw new ArgumentException("Customer id must be positive");
         }
+    }
 
+    void ValidatePlanCode(string planCode)
+    {
         if (string.IsNullOrWhiteSpace(planCode))
         {
             throw new ArgumentException("Plan code is required");
         }
+    }
 
+    void ValidateSeatCount(int seatCount)
+    {
         if (seatCount <= 0)
         {
             throw new ArgumentException("Seat count must be positive");
         }
+    }
 
+    void ValidatePaymentMethod(string paymentMethod)
+    {
         if (string.IsNullOrWhiteSpace(paymentMethod))
         {
             throw new ArgumentException("Payment method is required");
