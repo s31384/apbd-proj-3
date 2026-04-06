45472c92e7b6b7937b366ef5853e92c2486bb559
Artem Kraiushkin <s31384@pjwstk.edu.pl>
2026-04-06 13:49:22 +0300

    IDiscountStrategy interface and discount classes

---
+++ LegacyRenewalApp/IDiscountStrategy.cs
@@ -0,0 +1,58 @@
+﻿namespace LegacyRenewalApp;
+
+public interface ISegmentDiscountStrategy
+{
+    decimal GetDiscountAmount(decimal baseAmount, SubscriptionPlan subPlan);
+    string GetNotes(string notes);
+}
+
+
+public class SilverSegment : ISegmentDiscountStrategy
+{
+    public decimal GetDiscountAmount(decimal baseAmount,  SubscriptionPlan subPlan)
+    {
+        return baseAmount * 0.05m;
+    }
+
+    public string GetNotes(string notes)
+    {
+        return notes += " silver discount; ";
+    }
+}
+public class GoldSegment : ISegmentDiscountStrategy
+{
+    public decimal GetDiscountAmount(decimal baseAmount, SubscriptionPlan subPlan)
+    {
+        return baseAmount * 0.1m;
+    }
+
+    public string GetNotes(string notes)
+    {
+        return notes += " gold discount; ";
+    }
+}
+public class PlatinumSegment : ISegmentDiscountStrategy
+{
+    public decimal GetDiscountAmount(decimal baseAmount, SubscriptionPlan subPlan)
+    {
+        
+        return baseAmount * 0.15m;
+    }
+
+    public string GetNotes(string notes)
+    {
+        return notes += " platinum discount; ";
+    }
+}
+public class EducationSegment : ISegmentDiscountStrategy
+{
+    public decimal GetDiscountAmount(decimal baseAmount, SubscriptionPlan subPlan)
+    {
+        return baseAmount * 0.2m;
+    }
+
+    public string GetNotes(string notes)
+    {
+        return notes += " education discount; ";
+    }
+}
